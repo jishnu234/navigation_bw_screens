c05dd10fc908d36043e2f25cf96de58ab09dd54e
Jishnu M <jishnuvedhikz88@gmail.com>
2021-06-24 22:50:37 +0530

    Added named rooutes using generator class

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     title: 'Navigation',
     // initialRoute: '/',
-    home: FirstRoute(),
-    routes: {
-      '/second': (_) => SecondRoute(),
-    },));
+    initialRoute: '/',
+    onGenerateRoute: RouteGenerator.generateRoute,
+  ));
 }
 
 class FirstRoute extends StatelessWidget {
@@ -16,36 +14,82 @@ class FirstRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        appBar: AppBar(
-          title: Text('First Page'),
-        ),
-        body: Center(
-          child: ElevatedButton(child: Text('Next'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Next'),
           onPressed: () {
-            Navigator.of(context).pushNamed('/second');
-          },),
+            Navigator.of(context).pushNamed('/second',
+            arguments: 'Hello Jishnu from first page');
+          },
         ),
-      );
+      ),
+    );
   }
 }
 
-
 class SecondRoute extends StatelessWidget {
-  const SecondRoute({Key? key}) : super(key: key);
+  const SecondRoute({Key? key, required this.args}) : super(key: key);
+
+  final String args;
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        appBar: AppBar(
-          title: Text('Second Page'),
-        ),
-        body: Center(
-          child: ElevatedButton(child: Text('back'),
-          onPressed: () {
-            Navigator.pop(context);
-          },),
-        ),);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Container(
+       child:
+          Center(
+            child: Text(
+              args,
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 25,
+              ),
+            ),
+          ), 
+      ),
+    );
   }
 }
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+
+    final args = settings.arguments;
+
+    switch(settings.name) {
+      case '/':
+      return MaterialPageRoute(builder: (_) => FirstRoute());
+      case '/second': 
+      if(args is String) {
+        return MaterialPageRoute(
+          builder: (_) => SecondRoute(args: args)
+        );
+      }
+
+      return _errorRoute();
+
+      default: 
+      return _errorRoute();
+    }
+  }
+
+ static Route<dynamic> _errorRoute() {
+   return MaterialPageRoute(builder: (_){
+      return Scaffold(
+        appBar: AppBar(title: Text('Error'),),
+        body: Center(
+          child: Text('ERROR'),
+        )
+      );
+   });
+ }
+}
+
 
